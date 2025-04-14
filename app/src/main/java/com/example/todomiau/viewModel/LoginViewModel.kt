c1dd93e83a65776d72eb460c5127bec4151ac166
Diego <diegosoto@comunidad.unam.mx>
2025-04-14 02:15:09 -0600

    Adding Publishers

--- app/src/main/java/com/example/todomiau/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/todomiau/viewModel/LoginViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.todomiau.viewModel
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LoginViewModel: ViewModel() {
+    private val _loaderState = MutableLiveData<Boolean>()
+    val loaderState: LiveData<Boolean>
+        get() = _loaderState
+    private val _sessionValid = MutableLiveData<Boolean>()
+    val sessionValid: LiveData<Boolean>
+        get() = _sessionValid
+    private val firebase = FirebaseAuth.getInstance()
 }
--- app/src/main/java/com/example/todomiau/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/example/todomiau/viewModel/RegisterViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.todomiau.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
-class RegisterViewModel {
 
+class RegisterViewModel: ViewModel() {
+        private val _loaderState = MutableLiveData<Boolean>()
+        val loaderState: LiveData<Boolean>
+            get() = _loaderState
+        private val firebase = FirebaseAuth.getInstance()
 }
