0b148e51df603560ccc7df818bd68681c7ad893d
Diego <diegosoto@comunidad.unam.mx>
2025-04-14 01:42:55 -0600

    Adding Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.lottie)
+    // Firebase
+    implementation(platform(libs.firebase))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
 }
